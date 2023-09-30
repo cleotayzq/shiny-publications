c2f9dd237fe559e4bf3d8facd923af9e0a944ef5
cleotayzq <63363637+cleotayzq@users.noreply.github.com>
2023-09-29 21:10:38 -0400

    Create app.R

---
+++ yayaati/app.R
@@ -0,0 +1,152 @@
+# written by cleo tay
+# last updated 2023-09-29
+
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(dplyr)
+library(stringr)
+
+# import publication data
+publications <- read.csv("list_current.csv")
+
+coauthors <- publications$author %>%
+    strsplit(., " and ") %>%
+    unlist() %>%
+    subset(., . != "Chachan, Yayaati") %>%
+    table() %>%
+    # unique() %>%
+    sort(decreasing = T) %>%
+    as.data.frame() %>%
+    rowwise() %>%
+    filter(Freq >= 4 | # including authors who've coauthored at least 4 papers
+               # and a few more
+               `.` == "Stevenson, David J." |
+               `.` == "Clarke, Cathie" |
+               `.` == "Zhang, Michael") %>% 
+    select(1) %>%
+    pull()
+
+# pull keywords from bibtex file
+keywords <- publications$keywords %>%
+    strsplit(., ", ") %>%
+    unlist() %>%
+    unique() %>%
+    subset(., !str_detect(., "\\d+")) %>%
+    sort() %>%
+    tolower()
+
+recode_authorship <- function(input_string) {
+    if (input_string == "First") {
+        output_int <- 1
+    } else if (input_string == "Second") {
+        output_int <- 2
+    } else if (input_string == "Third") {
+        output_int <- 3
+    } else if (input_string == "&#8805; Fourth") {
+        output_int <- 4
+    }
+    return(output_int)
+}
+
+# Define UI for application
+ui <- fluidPage(
+    theme = shinytheme("darkly"),
+    
+    titlePanel("Publications"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            fluidRow(column(width = 12,
+                    sliderInput("year_range", "Filter by year:", 
+                                min = min(publications$year),
+                                max = max(publications$year),
+                                value = c(min(publications$year), max(publications$year)),
+                                step = 1, round = TRUE, sep = ""),
+                    sliderTextInput("authorship", "Filter by authorship order:",
+                                    choices = c("First", "Second", "Third", "&#8805; Fourth"),
+                                    selected = c("First", "&#8805; Fourth"),
+                                    grid = TRUE)
+                    # actionButton("show_all", "Show all papers") # moved to main panel
+            )),
+            fluidRow(column(width = 12,
+                            checkboxGroupButtons("keyword_filter", "Filter by keyword(s):",
+                                                 choices = keywords, selected = keywords,
+                                                 direction = "horizontal", size = "xs"),
+                            actionButton("no_keywords", "Clear keywords")
+            )),
+            br(),
+            fluidRow(column(width = 12,
+                checkboxGroupButtons("coauthor_filter", "Filter by co-author(s):",
+                                     choices = coauthors, selected = coauthors,
+                                     direction = "horizontal", size = "xs"),
+                # checkboxGroupInput("coauthor_filter", "Filter by co-author(s):",
+                #                      choices = coauthors, selected = coauthors),
+                actionButton("no_coauthors", "Clear co-authors")
+            ))
+        ),
+        
+        mainPanel(
+            fixedRow(column(width = 12, offset = 1.5,
+                            actionButton("show_all", "Show all papers"))
+            ),
+            br(),
+            htmlOutput("filtered_citations")
+        )
+    )
+)
+
+# Define server logic
+server <- function(input, output, session) {
+    
+    filtered_data <- reactive({
+        
+        nyay_min <- recode_authorship(input$authorship[1])
+        nyay_max <- recode_authorship(input$authorship[2]) %>%
+            ifelse(. == 4, 9, .)
+        
+        filtered <- publications %>%
+            mutate(apacitation = gsub("\\\\\\&", "&", apacitation)) %>%
+            filter(year >= input$year_range[1] & year <= input$year_range[2],
+                   nyayaati >= nyay_min & nyayaati <= nyay_max) %>%
+            rowwise() %>%
+            filter(any(input$keyword_filter %in% tolower(unlist(strsplit(keywords, ", "))))) %>%
+            filter(any(input$coauthor_filter %in% unlist(strsplit(author, " and ")))) %>%
+            ungroup()
+                   
+        return(filtered$apacitation)
+    })
+
+    output$filtered_citations <- renderUI({ 
+        filtered <- filtered_data()
+        
+        if (length(filtered) == 0) {
+            return("No matching publications")
+        } else {
+            # Create a list of HTML elements for each citation
+            citation_list <- paste(filtered, collapse = "</li><br><li>") %>%
+                paste("<ol><li>", ., "</li></ol>") %>%
+                HTML()
+            return(citation_list)
+        }
+    })
+    
+    # Observe the Reset Filters button click and reset inputs
+    observeEvent(input$show_all, {
+        updateSliderInput(session, "year_range", value = c(min(publications$year), max(publications$year)))
+        updateSliderTextInput(session, "authorship", selected = c("First", "&#8805; Fourth"))
+        # updateCheckboxGroupInput(session, "coauthor_filter", selected = coauthors)
+        updateCheckboxGroupButtons(session, "coauthor_filter", selected = coauthors)
+        updateCheckboxGroupButtons(session, "keyword_filter", selected = keywords)
+    })
+    observeEvent(input$no_coauthors, {
+        updateCheckboxGroupButtons(session, "coauthor_filter", selected = "")
+        updateCheckboxGroupInput(session, "coauthor_filter", selected = "")
+    })
+    observeEvent(input$no_keywords, {
+        updateCheckboxGroupButtons(session, "keyword_filter", selected = "")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
